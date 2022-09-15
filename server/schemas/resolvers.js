509bc1afe9cd7437446c828efe596daef985348d
kate patch <kate.epatch@gmail.com>
2022-09-14 21:28:50 -0600

    added missing part

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -59,14 +59,15 @@ const resolvers = {
 
         removeBook: async (parent, args, context) => {
             if(context.user) {
-                const updatedUser = await User.findByIdAndUpdate(
+                const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
                     { $pull: {
                         savedBooks: {
                             bookId: args.bookId
                             }
                         }
-                    }
+                    },
+                    { new: true }
                 );
                 return updatedUser;
             }
