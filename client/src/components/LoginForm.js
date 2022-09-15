bb8dba2d964cab7754f73a84ce4591535c485c4c
kate patch <kate.epatch@gmail.com>
2022-09-14 21:50:13 -0600

    missing syntax.  also polished up commented out code to clean up file

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -21,25 +21,12 @@ const LoginForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
-    // const form = event.currentTarget;
-    // if (form.checkValidity() === false) {
-    //   event.preventDefault();
-    //   event.stopPropagation();
-    // }
-
     try {
       const { data } = await loginUser({
         variables: {...userFormData}
       });
-
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
-
-      // const { token, user } = await response.json();
-      // console.log(user);
-      Auth.login(token);
+  
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
