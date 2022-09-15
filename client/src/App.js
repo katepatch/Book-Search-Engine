598b3ced506595b3b52b02555b7fffa7c89070a5
kate patch <kate.epatch@gmail.com>
2022-09-14 21:46:15 -0600

    refactored the apolloclient function again to polish it up

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,18 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemomryCache(),
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+    
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
 });
-
 
 function App() {
   return (
