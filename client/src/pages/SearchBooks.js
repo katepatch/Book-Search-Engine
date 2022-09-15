5752be381b4b5947723081b82536ebefb41c73d2
kate patch <kate.epatch@gmail.com>
2022-09-14 20:59:09 -0600

    fixed spacing

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -68,7 +68,7 @@ const SearchBooks = () => {
 
     try {
       const { data } = await saveBook({
-        variables: { input: bookInput}
+        variables: { input: bookInput }
       });
 
       if (error) {
