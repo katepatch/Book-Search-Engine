eee45fee6d3770a0d30dc79b9ac26f87030c2c60
kate patch <kate.epatch@gmail.com>
2022-09-15 21:50:37 -0600

    trying something new to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -23,14 +23,15 @@ app.use(express.json());
 
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static('../client/build'));
+  //app.use(routes);
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '../client/build/index.html'));
+  });
 }
 
-//app.use(routes);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+
 
 
 db.once('open', () => {
