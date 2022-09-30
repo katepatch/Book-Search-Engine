de0138782c4e6abdd45f157158f8ce7a42ce6694
kate patch <kate.epatch@gmail.com>
2022-09-29 23:11:17 -0600

    formatting

--- server/index.js
+++ server/index.js
@@ -27,9 +27,9 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
   app.use(express.static('../client/build'));
   //app.use(routes);
 }
-  app.get('*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '../client/build/index.html'));
-  });
+});
 
 
 // mongoose.connect(
