42ce1f3a76e096b31a26d351dbea33e99abed610
kate patch <kate.epatch@gmail.com>
2022-09-29 22:04:11 -0600

    mutation typedef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,7 +39,7 @@ const typeDefs = gql`
     type Mutations {
        login(email: String!, password: String!): Auth
        addUser(username: String!, email: String!, password: String!): Auth
-       saveBook(input: bookInput): User
+       saveBook(input: String!): User
        removeBook(input: String!): User
     }
 `;
