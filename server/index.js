2baba8cbd7582de39b246773e53216fa3554e4f9
kate patch <kate.epatch@gmail.com>
2022-09-29 23:10:57 -0600

    moving curly bracket

--- server/index.js
+++ server/index.js
@@ -26,10 +26,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static('../client/build'));
   //app.use(routes);
+}
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '../client/build/index.html'));
   });
-}
+
 
 // mongoose.connect(
 //   process.env.MONGODB_URI || 'mongodb://localhost:27017/BookSearch',
