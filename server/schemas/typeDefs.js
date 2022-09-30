1f15de882abcb96486aa7f4221fafff49fc98a4a
kate patch <kate.epatch@gmail.com>
2022-09-29 21:58:40 -0600

    added input for book input typedef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -18,6 +18,15 @@ const typeDefs = gql`
         link: String
     }
 
+    input bookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+      }
+
     type Auth {
         token: ID
         user: User
