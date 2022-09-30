99990c8ce1fdc92410733d8b1c786da890668a00
kate patch <kate.epatch@gmail.com>
2022-09-30 13:17:07 -0600

    got a not found error when i tried that

--- server/index.js
+++ server/index.js
@@ -24,10 +24,10 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static('../../client/build'));
   //app.use(routes);
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '../client/build/index.html'));
+    res.sendFile(path.join(__dirname + '../../client/build/index.html'));
  });
 }
 
