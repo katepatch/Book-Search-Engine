b4185c010f12e77acbe517991aa089a2fe956bd2
kate patch <kate.epatch@gmail.com>
2022-10-01 17:20:28 -0600

    trying to figure out the connection to heroku and mongodb

--- server/index.js
+++ server/index.js
@@ -24,12 +24,13 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../../client/build'));
-  //app.use(routes);
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '../../client/build/index.html'));
- });
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
+  //app.use(routes);
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/'));
+  })
+
 
 
 const startApolloServer = async (typeDefs, resolvers) => {
